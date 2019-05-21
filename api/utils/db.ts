57488b45fb8fbe7465a2247e26099f2f1420a2dc
mhma <mhma@cookapps.com>
2019-05-21 12:29:36 +0900

    feat: add db helper

--- api/utils/db.ts
+++ api/utils/db.ts
@@ -1,6 +1,6 @@
-import { createConnection } from 'mysql2/promise'
+import { createConnection, escapeId } from 'mysql2/promise'
 import config from '../config'
-
+// a
 class DBManager {
   private connections: Object = {}
 
@@ -26,6 +26,35 @@ class DBManager {
   /**
    * Exrtract Helpers
    */
+  async query (sql, params: any[] = [], dbConfigName: string = 'default'): Promise<any> {
+    const conn = await this.getConnection(dbConfigName)
+    const ret = await conn.query(sql, params)
+    return ret
+  }
+
+  async insert (table, record, ignore: boolean = false) {
+    let query = 'INSERT' + (ignore ? ' IGNORE' : '') + ' INTO ' + escapeId(table) + ' SET ?'
+    const res = await this.query(query, record)
+    return res[0].insertId
+  }
+
+  async update (table, index, record) {
+    let query = 'UPDATE ' + escapeId(table) + ' SET ? WHERE'
+    let values = [record]
+    if (typeof index === 'object') {
+      for (let i in index) {
+        query += ' ' + escapeId(i) + ' = ? AND'
+        values.push(index[i])
+      }
+      query = query.substr(0, query.length - 4)
+    } else {
+      query += ' `id` = ?'
+      values.push(index)
+    }
+    const res = await this.query(query, values)
+    return res
+  }
+
   async fetchRows (sql, params: any[] = [], dbConfigName: string = 'default'): Promise<any[]> {
     const conn = await this.getConnection(dbConfigName)
     const queryResult = await conn.query(sql, params)
